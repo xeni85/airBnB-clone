f510abe3b4496ccbdf2426846e607bb08559cb29
xeni85 <shkelzen.dunisha@umsh.edu.al>
2023-04-19 18:33:21 -0400

    still working on the placespage

--- client/src/pages/PlacesPage.js
+++ client/src/pages/PlacesPage.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const PlacesPage = () => {
   return (
     <div>
+        <div className='places-container'>
+            <Link></Link>
+        </div>
         My places
     </div>
   )
