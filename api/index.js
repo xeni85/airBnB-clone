f73ecd893979ee0334adfe5f7ca628f9808b3996
xeni85 <shkelzen.dunisha@umsh.edu.al>
2023-04-19 10:11:23 -0400

    having minor issues

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const User = require('./models/User.js');
 const bcrypt = require('bcryptjs');
-const bSalt = bcrypt.genSalt(10);
+const bSalt = bcrypt.genSaltSync(10);
 
 require('dotenv').config()
 
@@ -22,13 +22,13 @@ app.get('/test', (req, res) => {
     res.json('test OK')
 })
 
-app.post('/register', (req, res) => {
+app.post('/register',  async (req, res) => {
     const {name, email, password} = req.body;
-    res.json({name,email,password})
-    User.create({
+    const user = await User.create({
         name,
         email,
         password: bcrypt.hashSync(password, bSalt),
     })
+    res.json(user)
 })
 app.listen(4000);
