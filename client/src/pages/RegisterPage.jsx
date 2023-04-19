d45cce363e31f269db1c590d8d6b045026b58846
xeni85 <shkelzen.dunisha@umsh.edu.al>
2023-04-18 21:25:03 -0400

    started work on the backend

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -4,14 +4,15 @@ import { Link } from 'react-router-dom'
 const RegisterPage = () => {
   return (
     <div className='login-form-container'>
-        <h1 className='login-title'>Login</h1>
+        <h1 className='login-title'>Register</h1>
         <form action="" className='login-form'>
+            <input type="text" placeholder='name'/>
             <input type="email" placeholder='email'/>
             <input type="password" placeholder='password'/>
             <button>Login</button>
             <div className='register-container'>
-                Don't have an account yet?
-                <Link to={'/register'} className='register-link'>Register now</Link>
+                Do you already have an account?
+                <Link to={'/login'} className='register-link'>Login</Link>
             </div>
         </form>
     </div>
