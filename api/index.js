c7f945e936d0359b4440e8395785c044417b1b0f
xeni85 <shkelzen.dunisha@umsh.edu.al>
2023-04-19 08:41:34 -0400

    working on mongoose

--- api/index.js
+++ api/index.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
+app.use(express.json());
+app.use(cors({
+    credentials: true,
+    origin: 'http://127.0.0.1:5173',
+}));
+
+//
 app.get('/test', (req, res) => {
     res.json('test OK')
 })
 
+app.post('/register', (req, res) => {
+    const {name, email, password} = req.body;
+    res.json({name,email,password})
+})
 app.listen(4000);
--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,18 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
 
 const RegisterPage = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    function register(e) {
+        e.preventDefault();
+        axios.post('/register', {
+            name,
+            email,
+            password,
+        })
+    }
   return (
     <div className='login-form-container'>
         <h1 className='login-title'>Register</h1>
-        <form action="" className='login-form'>
-            <input type="text" placeholder='name'/>
-            <input type="email" placeholder='email'/>
-            <input type="password" placeholder='password'/>
-            <button>Login</button>
+        <form action="" className='login-form' onClick={register}>
+            <input type="text" placeholder='name' value={name} onChange={e => setName(e.target.value)}/>
+            <input type="email" placeholder='email'value={email} onChange={e => setEmail(e.target.value)}/>
+            <input type="password" placeholder='password'value={password} onChange={e => setPassword(e.target.value)}/>
+            <button>Register</button>
             <div className='register-container'>
                 Do you already have an account?
-                <Link to={'/login'} className='register-link'>Login</Link>
+                <Link to={'/login'} className='register-link'>Register</Link>
             </div>
         </form>
     </div>
