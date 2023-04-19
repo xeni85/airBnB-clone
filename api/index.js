96008e792dbabe0c78de786ccfabea914126144a
xeni85 <shkelzen.dunisha@umsh.edu.al>
2023-04-19 10:29:47 -0400

    still working on solvign axios issues

--- api/index.js
+++ api/index.js
@@ -2,13 +2,14 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
-const User = require('./models/User.js');
 const bcrypt = require('bcryptjs');
-const bSalt = bcrypt.genSaltSync(10);
+const User = require('./models/User.js');
+
 
 require('dotenv').config()
 
 
+const bSalt = bcrypt.genSaltSync(10);
 app.use(express.json());
 app.use(cors({
     credentials: true,
@@ -24,11 +25,15 @@ app.get('/test', (req, res) => {
 
 app.post('/register',  async (req, res) => {
     const {name, email, password} = req.body;
-    const user = await User.create({
+    try {const user = await User.create({
         name,
         email,
         password: bcrypt.hashSync(password, bSalt),
     })
     res.json(user)
+    } catch (err) {
+        res.status(422).json(err);
+    }
+    
 })
 app.listen(4000);
