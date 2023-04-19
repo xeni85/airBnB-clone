a2819aaccf6075feffc73bfc091e4ac92a6c8d58
xeni85 <shkelzen.dunisha@umsh.edu.al>
2023-04-19 16:10:55 -0400

    working on the account page

--- client/src/pages/AccountPage.jsx
+++ client/src/pages/AccountPage.jsx
@@ -1,11 +1,23 @@
 import React, {useContext} from 'react'
 import { UserContext } from '../components/UserContext'
+import {useNavigate, Link} from 'react-router-dom'
 
 const AccountPage = () => {
-  const {user} = useContext(UserContext)
-  return (
-    <div>Account page for {user.name}</div>
-  )
+  const {ready, user} = useContext(UserContext)
+  const navigate = useNavigate()
+    if(!ready) {
+      return 'Loading...'
+    }
+
+    if (ready && !user) {
+      return navigate('/login')
+    }
+    return (<div>
+      <nav>
+        <Link>
+        </Link>
+      </nav>
+    </div>)
 }
 
 export default AccountPage
