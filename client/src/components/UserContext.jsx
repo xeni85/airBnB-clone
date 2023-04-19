4056f666577019956f68c450b948272845a1189f
xeni85 <shkelzen.dunisha@umsh.edu.al>
2023-04-19 13:29:44 -0400

    working on the context some more

--- client/src/components/UserContext.jsx
+++ client/src/components/UserContext.jsx
@@ -1,10 +1,11 @@
-import React, { createContext } from 'react'
+import React, { createContext, useState } from 'react'
 
 export const UserContext = createContext({})
 
 export const UserContextProvider = (children) => {
+    const [user, setUser] = useState(null)
   return (
-    <UserContext.Provider value={{}}>
+    <UserContext.Provider value={{user, setUser}}>
         {children}
     </UserContext.Provider>
   )
