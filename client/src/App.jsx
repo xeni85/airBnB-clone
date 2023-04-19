51ecb2366799ab6e29c82da2417b14c42bfa6f75
xeni85 <shkelzen.dunisha@umsh.edu.al>
2023-04-19 17:30:12 -0400

    working on the accountpage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -19,9 +19,8 @@ function App() {
         <Route index element={<IndexPage />}/>
         <Route path='/login' element={<LoginPage />} />
         <Route path='/register' element={<RegisterPage />} />
-        <Route path='/account' element={<AccountPage />} />
-        <Route path='/account' element={<AccountPage />} />
-        <Route path='/account' element={<AccountPage />} />
+        <Route path='/account/:subpage?' element={<AccountPage />}></Route>
+
       </Route>
     </Routes>
     </UserContextProvider>
--- client/src/pages/AccountPage.jsx
+++ client/src/pages/AccountPage.jsx
@@ -1,6 +1,6 @@
 import React, {useContext} from 'react'
 import { UserContext } from '../components/UserContext'
-import {useNavigate, Link} from 'react-router-dom'
+import {useNavigate, Link, useParams} from 'react-router-dom'
 
 const AccountPage = () => {
   const {ready, user} = useContext(UserContext)
@@ -12,11 +12,19 @@ const AccountPage = () => {
     if (ready && !user) {
       return navigate('/login')
     }
+
+    let {subpage} = useParams();
+    if(subpage === undefined) {subpage = 'profile';}
+    function linkClasses (type=null) {
+      let classes = 'my-bookings';
+      if(type === subpage) {classes += ' my-profile'}
+      return classes
+    }
     return (<div>
       <nav className='account-links-container'>
-        <Link to={'/account'} className='my-profile'>My profile</Link>
-        <Link to={'/account/bookings'} className='my-bookings'>My bookings</Link>
-        <Link to={'/account/places'} className='my-places'>My places</Link>
+        <Link to={'/account'} className={linkClasses('profile')}>My profile</Link>
+        <Link to={'/account/bookings'} className={linkClasses('bookings')}>My bookings</Link>
+        <Link to={'/account/places'} className={linkClasses('places')}>My places</Link>
       </nav>
     </div>)
 }
