6cb37f6a65e34bf8200e5dff13fac11c5d3155be
xeni85 <shkelzen.dunisha@umsh.edu.al>
2023-04-19 15:49:53 -0400

    still working on it

--- client/src/pages/AccountPage.jsx
+++ client/src/pages/AccountPage.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import { UserContext } from '../components/UserContext'
 
 const AccountPage = () => {
+  const {user} = useContext(UserContext)
   return (
-    <div>Account page</div>
+    <div>Account page for {user.name}</div>
   )
 }
 
