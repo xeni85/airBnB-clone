7b5628e9ccc7edbdd94b1f4cbc56f1c96b461ed0
xeni85 <shkelzen.dunisha@umsh.edu.al>
2023-04-19 20:06:39 -0400

    working on the places form

--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -9,7 +9,13 @@ const PlacesPage = () => {
             <Link className='places-link-new my-profile my-bookings ' to={'/account/places/new'}>Add a place</Link>
         </div>)}
         {action === 'new' && (<div className='places-container'>
-            form
+            <form className='place-form'>
+              <h2>Title</h2>
+              <input type="text" placeholder='title, for example: Apartment'/>
+              <h2>Address</h2>
+              <input type="text" placeholder='address'/>
+              <h2>Photos</h2>
+            </form>
         </div>)}
         
     </div>
